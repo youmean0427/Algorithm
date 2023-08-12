55450f0227b0cbdbc429d9ee44ae16d0e36bbd86
YM <109326287+youmean0427@users.noreply.github.com>
2023-08-12 18:09:30 +0900

    [Silver I] Title: 회의실 배정, Time: 320 ms, Memory: 54868 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,16 +1,20 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
+
+T = int(input())
 time = []
-for i in range(n):
+for j in range(T):
     start, end = map(int, input().split())
     time.append([start, end])
 
-time=sorted(time, key=lambda x: (x[1],x[0])) # 회의가 끝나는 시간으로 오름차순 정렬 후 시작 시간이 빠른 것으로 오름차순 정렬
+time = sorted(time, key=lambda x: (x[1], x[0]))
+# print(time)
 
 count = 1
-end_time = time[0][1]
-for i in range(1,n):
-    if time[i][0] >= end_time:
-        count+=1
-        end_time = time[i][1]
+end = time[0][1]
+for i in range(1, T):
+    if time[i][0] >= end:
+        count += 1
+        end = time[i][1]
 
 print(count)
