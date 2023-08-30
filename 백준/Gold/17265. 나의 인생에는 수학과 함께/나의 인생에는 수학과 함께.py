1e978f2cf28e8e36f7168347f0cd87c093d2f2b4
YM <109326287+youmean0427@users.noreply.github.com>
2023-08-30 18:24:52 +0900

    [Gold V] Title: 나의 인생에는 수학과 함께, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/17265. 나의 인생에는 수학과 함께/나의 인생에는 수학과 함께.py
@@ -0,0 +1,57 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [[x for x in input().split()] for _ in range(N)]
+visited = [[set() for _ in range(N)] for _ in range(N)]
+visited[0][0].add(int(arr[0][0]))
+
+def bfs(arr, start, end):
+
+    q = [(start, end)]
+
+    while q:
+
+        x, y = q.pop()
+        dir = [(0, 1), (1, 0)]
+        dir_2 = [(0, -2), (-1, -1)]
+        dir_3 = [(-2, 0), (-1, -1)]
+        for n, m in dir:
+            nx = n + x
+            my = m + y
+
+            if 0 <= nx < N and 0 <= my < N:
+                if arr[nx][my].isdigit():
+                    if nx == x:
+                        for a, b in dir_2:
+                            if 0 <= a + nx < N and 0 <= b + my < N:
+                                if visited[a + nx][b + my]:
+                                    if arr[x][y] == '+':
+                                        visited[nx][my].add(min(visited[a+nx][b+my]) + int(arr[nx][my]))
+                                        visited[nx][my].add(max(visited[a+nx][b+my]) + int(arr[nx][my]))
+                                    elif arr[x][y] == '-':
+                                        visited[nx][my].add(min(visited[a+nx][b+my]) - int(arr[nx][my]))
+                                        visited[nx][my].add(max(visited[a+nx][b+my]) - int(arr[nx][my]))
+                                    elif arr[x][y] == '*':
+                                        visited[nx][my].add(min(visited[a+nx][b+my]) * int(arr[nx][my]))
+                                        visited[nx][my].add(max(visited[a+nx][b+my]) * int(arr[nx][my]))
+                    elif my == y:
+                        for a, b in dir_3:
+                            if 0 <= a + nx < N and 0 <= b + my < N:
+                                if visited[a + nx][b + my]:
+                                    if arr[x][y] == '+':
+                                        visited[nx][my].add(min(visited[a + nx][b + my]) + int(arr[nx][my]))
+                                        visited[nx][my].add(max(visited[a + nx][b + my]) + int(arr[nx][my]))
+                                    elif arr[x][y] == '-':
+                                        visited[nx][my].add(min(visited[a + nx][b + my]) - int(arr[nx][my]))
+                                        visited[nx][my].add(max(visited[a + nx][b + my]) - int(arr[nx][my]))
+                                    elif arr[x][y] == '*':
+                                        visited[nx][my].add(min(visited[a + nx][b + my]) * int(arr[nx][my]))
+                                        visited[nx][my].add(max(visited[a + nx][b + my]) * int(arr[nx][my]))
+
+                    q.append((nx, my))
+                else:
+                    q.append((nx, my))
+
+bfs(arr, 0, 0)
+print(max(visited[-1][-1]), min(visited[-1][-1]))
