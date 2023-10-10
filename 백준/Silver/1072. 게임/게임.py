9895eb18ec3ec8ee1260b21f811bebf405ff729d
YM <109326287+youmean0427@users.noreply.github.com>
2023-10-11 01:48:55 +0900

    [Silver III] Title: 게임, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -2,17 +2,17 @@ import sys
 input = sys.stdin.readline
 
 X, Y = map(int, input().split())
+# 부동소수점 오차, int(Y/X*100)으로 작성 금지!!!
 Z = int((Y * 100 / X))
 
 start = 0
-end = 1000000000
+end = X  # Y의 범위 : 0 <= Y <= X
 z = 0
 result = -1
 
 while start <= end:
 
     mid = (start + end) // 2
-
     if Z < int(((Y + mid) * 100 / (X + mid))):
         result = mid
         end = mid - 1
