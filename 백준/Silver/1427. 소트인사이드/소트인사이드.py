145b14f6a6a0adc10db7f84cd15fc8e5daa13451
YM <109326287+youmean0427@users.noreply.github.com>
2023-10-28 00:01:37 +0900

    [Silver V] Title: 소트인사이드, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -7,34 +7,7 @@ arr = []
 for i in N:
     arr.append(int(i))
 
-def quick_sort(arr, start, end):
-
-    if start < end:
-        pivot = arr[start]
-        first = start
-        last = end
-
-        while start <= end:
-
-            # pivot보다 큰 수 찾기
-            while start <= end and arr[start] <= pivot:
-                start += 1
-            # pivot보다 작은 수 찾기
-            while start <= end and arr[end] >= pivot:
-                end -= 1
-
-            if start < end:
-                arr[start], arr[end] = arr[end], arr[start]
-
-        arr[first], arr[end] = arr[end], arr[first]
-
-        quick_sort(arr, first, end - 1)
-        quick_sort(arr, end + 1, last)
-
-        return arr[::-1]
-    return arr
-
-arr = quick_sort(arr, 0, len(arr)-1)
+arr.sort(reverse=True)
 
 result = ''
 for i in arr:
