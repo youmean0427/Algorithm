c8a32d7f074342f1aee00a086754f044c4a7b43f
YM <109326287+youmean0427@users.noreply.github.com>
2023-11-01 19:20:51 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 192 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(n, sm):
+    global max_val, min_val
+
+    if n == N-1:
+        max_val = max(max_val, sm)
+        min_val = min(min_val, sm)
+        return
+
+    if op[0]:
+        op[0] -= 1
+        dfs(n+1, sm + arr[n+1])
+        op[0] += 1
+
+    if op[1]:
+        op[1] -= 1
+        dfs(n+1, sm - arr[n+1])
+        op[1] += 1
+
+    if op[2]:
+        op[2] -= 1
+        dfs(n+1, sm * arr[n+1])
+        op[2] += 1
+
+    if op[3]:
+        op[3] -= 1
+        dfs(n+1, int(sm / arr[n+1]))
+        op[3] += 1
+
+N = int(input())
+arr = [int(x) for x in input().split()]
+op = [int(x) for x in input().split()]
+# 0 : +, 1 : -, 2 : x, 3 : //
+
+max_val = -1000000000
+min_val = 1000000000
+dfs(0, arr[0])
+print(max_val)
+print(min_val)
