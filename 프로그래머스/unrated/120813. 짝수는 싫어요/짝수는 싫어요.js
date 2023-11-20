9797a0b2a9b8aae4a8718d371143626ffe425be3
YM <109326287+youmean0427@users.noreply.github.com>
2023-11-20 18:10:05 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,11 +1,9 @@
 function solution(n) {
     var answer = [];
     let i = 1
-    let j = 0
     while (i <= n) {
-        (i % 2 == 0) ? j = 1 : answer.push(i)
+        (i % 2 == 0) ? i : answer.push(i)
         i += 1
     }
-    
     return answer;
 }
