32f092a9ef90e2a4aa4aef8cda5fdb5ee2c910f9
YM <109326287+youmean0427@users.noreply.github.com>
2023-11-23 21:34:46 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 4148 ms, Memory: 245952 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -2,37 +2,40 @@ import sys
 import heapq
 input = sys.stdin.readline
 
-def prim(x):
-    heap = []
-    heapq.heappush(heap, (0, x))
-    result = 0
-    while heap:
-        c, y = heapq.heappop(heap)
+def find_set(x):
+    if p[x] != x:
+        p[x] = find_set(p[x])
+    return p[x]
 
-        if visited[y] == 0:
-            visited[y] = 1
-            key[y] = c
-            result += c
-            for k in arr[y]:
-                if visited[k[1]] == 0:
-                    heapq.heappush(heap, (k[0], k[1]))
-
-    return result
+def union(x, y, c):
+    global result
+    if find_set(x) != find_set(y):
+        p[find_set(y)] = find_set(x)
+        result += z
+        key[y] = z
 
 N, M = map(int, input().split())
-arr = [[] * (N+1) for _ in range(N+1)]
-visited = [0] * (N+1)
+
+arr = []
 key = [0] * (N+1)
+
+p = [i for i in range(N+1)]
 
 for _ in range(M):
     a, b, c = map(int, input().split())
-    arr[b].append((c, a))
-    arr[a].append((c, b))
+    arr.append([a, b, c])
 
-result = prim(1)
+arr.sort(key = lambda x:x[2])
+
+cnt = 0
+result = 0
+while cnt < M:
+    x, y, z = arr[cnt][0], arr[cnt][1], arr[cnt][2]
+    union(x, y, z)
+    cnt += 1
+
 x = result
 for i in range(1, N+1):
     x = min(x, result - key[i])
 
 print(x)
-
