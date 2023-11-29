998eeeeda7769374638adaa30e40e095fdfe5e3a
YM <109326287+youmean0427@users.noreply.github.com>
2023-11-29 17:41:43 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.py
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.py
@@ -1,22 +1,17 @@
 import sys
-import math
 input = sys.stdin.readline
 
-
-def DFS(n, sm):
-    global ans
-
-    if n == Y:
-        ans = max(ans, sm)
-        return
-    elif n > Y:
-        return
-
-    DFS(n+1, math.trunc(sm+sm*0.05))
-    DFS(n+3, math.trunc(sm+sm*0.2))
-    DFS(n+5, math.trunc(sm+sm*0.35))
-
-ans = 0
 H, Y = map(int, input().split())
-DFS(0, H)
-print(ans)
+
+dp = [0] * (Y+1)
+dp[0] = H
+
+for i in range(1, Y+1):
+    dp[i] = max(dp[i], int(dp[i-1] * 1.05))
+
+    if i >= 3:
+        dp[i] = max(dp[i], int(dp[i-3]*1.20))
+    if i >= 5:
+        dp[i] = max(dp[i], int(dp[i-5]*1.35))
+
+print(max(dp))
