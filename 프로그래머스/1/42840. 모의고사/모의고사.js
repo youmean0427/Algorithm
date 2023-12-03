c7382af663fd0bcd6c9758611e196d3eba191796
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-03 21:49:51 +0900

    [level 1] Title: 모의고사, Time: 4.37 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,35 @@
+function solution(answers) { 
+    var answer = [0, 0, 0];
+    let one = [1, 2, 3, 4, 5];
+    let two = [2, 1, 2, 3, 2, 4, 2, 5];
+    let three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+    const ol = one.length;
+    const tl = two.length;
+    const thl = three.length;
+
+    const N = answers.length;
+
+    let i = 0;
+    while (i < N) {
+        if (answers[i] === one[i%ol]) {
+            answer[0] += 1;
+        }
+        if (answers[i] === two[i%tl]) {
+            answer[1] += 1;
+        }
+        if (answers[i] === three[i%thl]) {
+            answer[2] += 1;
+        }
+        i += 1;
+    }
+    
+    const max_val = Math.max(...answer);
+    var result = [];
+    for (let j = 0; j < answer.length; j++) {
+        if (answer[j] === max_val) {
+            result.push(j + 1);
+        }
+    }
+    return result;
+}
