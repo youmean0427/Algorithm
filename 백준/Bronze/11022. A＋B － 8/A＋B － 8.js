f96782730b74a570caf905adbd589df14374d60b
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-06 21:25:57 +0900

    [Bronze V] Title: A+B - 8, Time: 116 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,14 @@
+let fs = require('fs')
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n")
+// console.log(input)
+
+const N = input[0]
+let i = 1
+let answer = ''
+while (i <= N) {
+    const line = input[i].split(" ")
+    answer += "Case #" + i + ": " +  parseInt(line[0]) + " + " + parseInt(line[1]) + " = " + (parseInt(line[0]) + parseInt(line[1])) + "\n"
+    i++
+} 
+
+console.log(answer)
