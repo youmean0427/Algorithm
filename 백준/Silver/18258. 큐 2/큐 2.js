306e588a0f24c2c7aff902887c75c4d05b288cab
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-12 17:43:42 +0900

    [Silver IV] Title: 큐 2, Time: 2200 ms, Memory: 291392 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.js
+++ 백준/Silver/18258. 큐 2/큐 2.js
@@ -1,57 +1,79 @@
-let fs = require('fs');
-let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let fs = require('fs')
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n")
+// let input = fs.readFileSync("./EX.txt").toString().trim().split('\n')
 
-let q = [];
-let result = "";
+class Queue {
+    constructor() {
+        this.q = []
+        this.start = 0
+        this.end = 0
+    }
+    push(value) {
+        this.q[this.end] = value
+        this.end++
+    }
+    
+    pop() {
+        if (this.start < this.end) {
+            const x = this.q[this.start]
+            this.start++ 
+            return x
+        } else {
+            return -1
+        }
+    }
+    size() {
+        return this.end - this.start
+    }
+    empty() {
+        if (this.start < this.end) {
+            return 0
+        } else {
+            return 1
+        }
+    }
+    front() {
+        if (this.start < this.end) {
+            const x = this.q[this.start]
+            return x
+        } else {
+            return -1
+        }
+    }
+    back() {
+        if (this.start < this.end) {
+            const x = this.q[this.end-1]
+            return x
+        } else {
+            return -1
+        }
+    }
 
-let front = 0;
-let back = 0;
-
-for (let i = 1; i < parseInt(input[0]) + 1; i++) {
-    let line = input[i].split(" ");
-    let word = line[0].trim();
-
-    if (word === "push") {
-        q[back++] = parseInt(line[1]);
-        continue;
-    }
-    if (word === 'pop') {
-        if (front < back) {
-            const x = q[front++];
-            result += x + '\n';
-        } else {
-            result += "-1\n";
-        }
-        continue;
-    }
-    if (word === 'size') {
-        result += back - front + "\n";
-        continue;
-    }
-    if (word === 'empty') {
-        if (front < back) {
-            result += "0\n";
-        } else {
-            result += "1\n";
-        }
-        continue;
-    }
-    if (word === 'front') {
-        if (front < back) {
-            result += q[front] + "\n";
-        } else {
-            result += "-1\n";
-        }
-        continue;
-    }
-    if (word === 'back') {
-        if (front < back) {
-            result += q[back - 1] + "\n";
-        } else {
-            result += "-1\n";
-        }
-        continue;
-    }
 }
 
-console.log(result);
+let q = new Queue()
+let result = ""
+
+let start = 0
+let end = 0
+
+for(let i = 1; i < parseInt(input[0])+1; i++) {
+    let line = input[i].split(" ")
+    let word = line[0].trim()
+
+    if (word === 'push') {
+        q.push(parseInt(line[1]))
+    } else if (word === 'pop') {
+        result += q.pop() + "\n"
+    } else if (word === 'size') {
+        result += q.size()  + "\n"
+    } else if (word === 'empty') {
+        result += q.empty() + "\n"
+    } else if (word === 'front') {
+        result += q.front() + "\n"
+    } else if (word === 'back') {
+        result += q.back() + "\n"
+    }
+
+}
+console.log(result)
