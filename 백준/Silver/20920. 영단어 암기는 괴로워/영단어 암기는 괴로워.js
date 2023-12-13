fdcc56e73c98d6c47906f75a043acf4a7452ac50
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-13 18:41:46 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 460 ms, Memory: 64188 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.js
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.js
@@ -3,20 +3,29 @@ let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n")
 // let input = fs.readFileSync("./EX.txt").toString().trim().split('\n')
 
 const M = parseInt(input[0].split(" ")[1])
-let arr = new Map()
+let arr = {}
 for (let i = 1; i < input.length; i++) {
     let x = input[i].trim()
-
+    
     if (x.length >= M) {
-        arr.set(x, (arr.get(x) ?? 0 ) + 1)
+        if (x in arr) {
+            arr[x] += 1
+        } else {
+            arr[x] = 1
+        }
     }
 } 
 
-arr = [...arr.entries()].sort((x, y) => {
-    if (x[1] !== y[1]) return y[1] - x[1]
-    if (x[0].length !== y[0].length) return y[0].length - x[0].length
-    return x[0].localeCompare(y[0])
-}).map(x => x[0])
+arr = Object.entries(arr)
+arr.sort((x, y) => {
+    if (x[1] > y[1]) return -1
+    if (x[1] < y[1]) return 1
+    if (x[0].length > y[0].length) return -1
+    if (x[0].length < y[0].length) return 1
+    if (x[0] > y[0]) return 1
+    if (x[0] < y[0]) return -1
+    return 1
+})
 
-
+arr = arr.map(x => x[0])
 console.log(arr.join("\n"))
