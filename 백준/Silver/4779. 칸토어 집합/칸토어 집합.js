20495c431bd6fc69113d26f76ab1911bd568ad99
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-14 19:42:08 +0900

    [Silver III] Title: 칸토어 집합, Time: 280 ms, Memory: 58956 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.js
@@ -0,0 +1,23 @@
+let fs = require('fs')
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n")
+// let input = fs.readFileSync("./EX.txt").toString().trim().split('\n')
+
+input = input.map(x => parseInt(x))
+
+function cantor(x, start, end){
+    if (start < end) {
+        let mid = parseInt((end - start + 1) / 3)
+        for (let i = start + mid; i< start + 2 * mid; i++) {
+            x[i] = " "
+        }
+        cantor(x, start, start + mid - 1)
+        cantor(x, start + 2 * mid, end)
+    }
+}
+
+for(let i = 0; i < input.length; i++) {
+    let N = input[i]
+    let arr = new Array(3 ** N).fill('-')
+    cantor(arr, 0, arr.length-1)
+    console.log(arr.join(""))
+}
