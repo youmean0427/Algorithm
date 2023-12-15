14be2347e2aba4a83e22c3250b87107d7c0769db
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-15 18:54:27 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,26 @@
+import sys, math
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+result = []
+visited = [0] * (N+1)
+def nm(n, sm):
+
+    if n == M:
+        result.append(sm)
+        return
+
+    for i in range(1, N+1):
+        if visited[i] == 1:
+            continue
+        if sm and i < int(sm[-1]):
+            continue
+
+        visited[i] = 1
+        nm(n+1, sm + str(i))
+        visited[i] = 0
+
+nm(0, "")
+for i in result:
+    print(" ".join(i))
