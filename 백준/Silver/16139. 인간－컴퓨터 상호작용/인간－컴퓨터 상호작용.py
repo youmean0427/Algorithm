9ce865f565c676f43c9929fa9261218eca3d8d67
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-16 18:36:07 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 88 ms, Memory: 32600 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,28 @@
+import sys, copy
+input = sys.stdin.readline
+
+S = input().rstrip()
+N = int(input())
+arr = [{S[0] : 1} for _ in range(len(S)+1)]
+arr[-1] = {}
+
+for i in range(1, len(S)):
+    arr[i] = copy.deepcopy(arr[i-1])
+    if S[i] in arr[i]:
+        arr[i][S[i]] += 1
+    else:
+        arr[i][S[i]] = 1
+
+for _ in range(N):
+    a, start, end = input().split()
+    start = int(start)
+    end = int(end)
+
+    x = 0
+    y = 0
+    if a in arr[end]:
+        x = arr[end][a]
+    if a in arr[start-1]:
+        y = arr[start-1][a]
+
+    print(x - y)
