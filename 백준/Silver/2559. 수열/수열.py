9aed98ec6b03ddfaf83d961bfc9b34c15520d579
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-16 17:47:23 +0900

    [Silver III] Title: 수열, Time: 104 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -3,20 +3,13 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 arr = [int(x) for x in input().split()]
-result = -float('inf')
+result = sum(arr[:K])
 
 for x in range(1, N):
     arr[x] += arr[x-1]
 
-
-for y in range(K-1, N):
-    if y-K == -1:
-        z = arr[y]
-    else:
-        z = arr[y] - arr[y-K]
+for y in range(K, N):
+    z = arr[y] - arr[y-K]
     result = max(result, z)
-
-if N == K:
-    result = arr[K-1]
 
 print(result)
