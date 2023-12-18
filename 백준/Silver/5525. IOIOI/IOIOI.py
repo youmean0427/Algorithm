00a9bc7194a793b0a21a6494725a8e9de9483eb0
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-18 16:23:17 +0900

    [Silver I] Title: IOIOI, Time: 828 ms, Memory: 73608 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,24 +3,45 @@ input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
-char = input().rstrip()
-arr = []
+S = input().rstrip()
+P = 'IOI' + 'OI' * (N-1)
+K = [0] * len(P)
 
-for x in char:
-    arr.append(x)
+def pattern (S):
 
-IOI = ['IOI']
+    i = 0
+    j = 1
+    while j < len(S):
+        if S[i] == S[j]:
+            K[j] = i + 1
+            i += 1
+            j += 1
+        else:
+            j += 1
 
-for i in range(1, N):
-    IOI.append(IOI[i-1] + 'OI')
-
-check_IOI = []
-for j in IOI[N-1]:
-    check_IOI.append(j)
-
+pattern(P)
 cnt = 0
-for k in range(0, M-len(check_IOI)+1):
-    if check_IOI == arr[k:k+len(check_IOI)]:
-        cnt += 1
 
+def KMP(P, S):
+
+    global cnt
+    i = 0
+    j = 0
+    while i < len(S):
+
+        if S[i] == P[j]:
+            if j == len(P) - 1:
+                cnt += 1
+                j = K[j-1]
+            else:
+                i += 1
+                j += 1
+
+        else:
+            if j > 0:
+                j = K[j-1]
+            else:
+                i += 1
+
+KMP(P, S)
 print(cnt)
