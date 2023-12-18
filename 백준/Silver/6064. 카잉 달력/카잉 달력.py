23c8d2f42b46dd992f95a4579d6e707672ff329d
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-18 21:22:27 +0900

    [Silver I] Title: 카잉 달력, Time: 2596 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,11 +1,12 @@
 import sys
 input = sys.stdin.readline
+from math import lcm
 
 T = int(input())
 for _ in range(T):
     M, N, x, y = map(int, input().split())
     cnt = -1
-    for i in range(0, M*N+1, M):
+    for i in range(0, lcm(M, N), M):
         if ((i + x) - x) % M == 0 and ((i + x)-y) % N == 0:
             cnt = i+x
             break
