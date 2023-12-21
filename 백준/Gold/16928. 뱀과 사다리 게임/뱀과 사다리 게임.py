212caa125bf9288ca6913ff42d9355fe9388e9c4
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-21 19:19:21 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 68 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -3,22 +3,20 @@ input = sys.stdin.readline
 from collections import deque
 
 N, M = map(int, input().split())
-arr = [0] * (101)
+arr  = [0] * (107)
 
 for _ in range(N + M):
     x, y = map(int, input().split())
     arr[x] = y
 
-visited = [float('inf')] * (101)
+visited = [float('inf')] * (107)
 visited[1] = 0
-visited[0] = 0
 
 def bfs(start):
 
     q = deque([start])
 
     while q:
-
         x = q.popleft()
 
         if x == 100:
@@ -26,13 +24,13 @@ def bfs(start):
 
         for i in range(1, 7):
             y = x + i
-            if 0 <= y <= 100 and visited[y] > visited[x] + 1:
+            if visited[y] > visited[x] + 1:
+                visited[y] = min(visited[y], visited[x] + 1)
+
                 if arr[y]:
-                    if visited[arr[y]] > visited[x] + 1:
-                        visited[arr[y]] = visited[x] + 1
-                        q.append(arr[y])
+                    q.append(arr[y])
+                    visited[arr[y]] = min(visited[arr[y]], visited[x] + 1)
                 else:
-                    visited[y] = visited[x] + 1
                     q.append(y)
 
 bfs(1)
