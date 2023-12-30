06f8c49d3acfb9e38c1ecb1b825d30678abce7d4
YM <109326287+youmean0427@users.noreply.github.com>
2023-12-30 16:25:13 +0900

    [Silver III] Title: 주유소, Time: 332 ms, Memory: 44600 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -3,15 +3,15 @@ let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n")
 // let input = fs.readFileSync("./EX.txt").toString().trim().split("\n")
 
 const N = parseInt(input[0])
-const road = input[1].split(" ").map(x => parseInt(x))
-const cost = input[2].split(" ").map(x => parseInt(x))
+const road = input[1].split(" ").map(x => (BigInt(x)))
+const cost = input[2].split(" ").map(x => (BigInt(x)))
 
 let minCost = cost[0]
 let total = minCost * road[0]
 
 for (let i = 1; i < N-1; i++) {
-    minCost = Math.min(minCost, cost[i])
+    minCost = minCost < cost[i] ? minCost : cost[i]
     total += minCost * road[i]
 }
 
-console.log(total)
+console.log(total.toString())
