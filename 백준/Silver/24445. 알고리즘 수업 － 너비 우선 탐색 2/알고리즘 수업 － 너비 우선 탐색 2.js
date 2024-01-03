4e9f3ba190b27a4ac289c57d911c3f23f40516b0
YM <109326287+youmean0427@users.noreply.github.com>
2024-01-03 18:55:55 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 2324 ms, Memory: 84444 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.js
@@ -0,0 +1,42 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+// let input = fs.readFileSync('./EX.txt').toString().trim().split('\n')
+
+const [N, M, R] = input[0].split(" ").map(x => parseInt(x))
+let arr = Array.from({length : N+1}, () => [])
+
+for (let i = 1; i <= M; i++) {
+    const [a, b] = input[i].split(" ").map(x => parseInt(x))
+    arr[a].push(b)
+    arr[b].push(a)
+}
+
+function bfs(x) {
+    
+    let visited = Array(N+1).fill(0)
+
+    let q = [x]
+    let cnt = 1
+    visited[x] = 1
+    while (q.length) {
+        let y = q.shift()
+
+        arr[y].sort((a, b) => b - a)
+        // console.log(arr[y])
+        arr[y].forEach(x => {
+            if (visited[x] === 0) {
+                visited[x] = cnt + 1
+                q.push(x)
+                cnt += 1
+            }
+        })
+    }
+    return visited
+}
+
+result = bfs(R)
+ans = ''
+for (let i = 1; i < result.length; i++) {
+    ans += result[i] + '\n'
+}
+console.log(ans)
