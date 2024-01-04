801b33eadec7e3fcac0f6ae49ba001c0fe111248
YM <109326287+youmean0427@users.noreply.github.com>
2024-01-04 22:57:11 +0900

    [Bronze III] Title: 곱셈, Time: 88 ms, Memory: 12340 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val one = readln().toInt()
+    val two = readln()
+
+    println(one * two[2].toString().toInt())
+    println(one * two[1].toString().toInt())
+    println(one * two[0].toString().toInt())
+    println(one * two.toInt())
+
+
+
+}
