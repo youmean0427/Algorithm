2b142cd65d4393784b99573b4e6f64d1f7ff4c94
YM <109326287+youmean0427@users.noreply.github.com>
2024-01-09 22:24:28 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let odd = 0
+    let even = 0
+    for (let i = 0; i < num_list.length; i++) {
+        if (i % 2 === 0) {
+            odd += num_list[i]         
+        } else {
+            even += num_list[i]
+        }
+    }
+    
+    var answer = Math.max(odd, even)
+    return answer;
+}
