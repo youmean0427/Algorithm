3abc0022eba64877431d939c54633eedad6dbbf7
YM <109326287+youmean0427@users.noreply.github.com>
2024-01-10 21:40:57 +0900

    [level 2] Title: 구명보트, Time: 8.56 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,21 @@
+def solution(people, limit):
+    cnt = 0
+    people.sort()
+    
+    i = 0
+    j = len(people)-1
+    
+    while i <= j:
+        if i == j:
+            cnt += 1
+            break
+        
+        elif people[i] + people[j] <= limit:
+            cnt += 1
+            j -= 1
+            i += 1
+        else:
+            cnt += 1
+            j -= 1
+    
+    return cnt
