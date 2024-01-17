e1f47931bbc6e7fbd1af00d362e1623ac5057318
YM <109326287+youmean0427@users.noreply.github.com>
2024-01-17 21:40:38 +0900

    [Silver III] Title: N과 M (5), Time: 260 ms, Memory: 26168 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -0,0 +1,29 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+// let input = fs.readFileSync('./EX.txt').toString().trim().split("\n")
+
+const [n, m] = input[0].split(" ").map((x) => parseInt(x))
+const arr = input[1].split(" ").map(x => parseInt(x))
+let visited = new Array(n).fill(0)
+arr.sort((a, b) => a - b)
+result = ""
+
+function dfs(x, sm){
+    
+    if (sm.length === m) {
+        result += sm.join(" ") + '\n'
+        return
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (visited[i] === 0) {
+            visited[i] = 1
+            dfs(i, [...sm , arr[i].toString()])
+            visited[i] = 0
+        }
+    }
+
+}
+
+dfs(0, [])
+console.log(result)
