ba1ab0d49371b18b9bd082e34629838aa01b191d
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-04 21:02:55 +0900

    [level 2] Title: 방문 길이, Time: 3.66 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,39 +1,37 @@
 def solution(dirs):
     global answer
-    arr = [[0 for _ in range(11)] for _ in range(11)]
     
-    arr[5][5] = 1
+    arr = [[0 for _ in range(11)] for _ in range(11)]
     sn, sm = 5, 5
     dir = list(dirs)
-
-    visited = [[0 for _ in range(11)] for _ in range(11)]
+    
     dir_dict = {"U" : [-1, 0], "D" : [1, 0], "R" : [0, 1], "L" : [0, -1]}
     answer = 0
     
     def dfs(sn, sm):
         global answer
+        
         road = []
         stack = [(sn, sm, 0)]
+        
         while stack:
             x, y, z = stack.pop()
 
             if z >= len(dir):
                 return road
+            
             n, m = dir_dict[dir[z]]
-            xn = x + n
-            ym = y + m
+            xn, ym = x + n, y + m
             z += 1
 
             if 0 <= xn < 11 and 0 <= ym < 11:
                 stack.append((xn, ym, z))
-                visited[xn][ym] += 1
                 if [(x, y), (xn, ym)] in road or [(xn, ym), (x, y)] in road:
                     continue
                 else:
                     road.append([(x, y), (xn, ym)])
             else:
                 stack.append((x, y, z))
-        return road
     
     x = dfs(sn, sm)
     answer = len(x)
