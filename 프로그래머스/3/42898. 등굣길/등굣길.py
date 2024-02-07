5bd501dfc2482f35a713ce53fafef040745ee374
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-07 19:25:22 +0900

    [level 3] Title: 등굣길, Time: 1.72 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,18 @@
+def solution(m, n, puddles):
+    
+    arr = [[0] * (m+1) for _ in range(n+1)]
+
+    if len(puddles):
+        for x, y in puddles:
+            arr[y][x] = 1 
+    
+    dp = [[0] * (m+1) for _ in range(n+1)]
+    dp[1][0] = 1
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if arr[i][j] == 0:
+                dp[i][j] = dp[i][j-1] + dp[i-1][j]
+            
+    answer = dp[-1][-1] % 1000000007
+    return answer
