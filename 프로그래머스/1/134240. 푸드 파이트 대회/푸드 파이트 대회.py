19ab712adf000a0ca5188997ffdf899cd116b906
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-10 14:21:37 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,19 +2,10 @@ def solution(food):
     answer = ''
     
     for i in range(len(food)):
-        if food[i] > 1:
-            food[i] = food[i] // 2 
-        elif food[i] <= 1:
-            food[i] = 0
-     
-    arr = []
-    for i in range(len(food)):
+        food[i] = int(food[i] / 2) 
         while food[i]:
-            arr.append(str(i))
+            answer += str(i)
             food[i] -= 1
     
-    answer = "".join(arr)  + "0"
-    arr.reverse()
-    answer += "".join(arr)
-    
+    answer = answer + '0' + answer[::-1]
     return answer
