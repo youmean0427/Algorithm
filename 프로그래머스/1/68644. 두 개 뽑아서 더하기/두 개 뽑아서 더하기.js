9ff9c9b3c4abd7ff46a9b8a4f11c66c5fa5847fc
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-11 10:45:27 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.51 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -5,17 +5,12 @@ function solution(numbers) {
     let ans = []
     for (let i = 0; i < numbers.length; i++) {
         for (let j = i+1; j < numbers.length; j++) {
-            answer.push([numbers[i], numbers[j]])
+            answer.push(numbers[i] + numbers[j])
         }
     }
     
-    answer.forEach(x => {
-        let res = x.reduce((a, b) => a += b)
-        if (!ans.includes(res)) {
-            ans.push(res)
-        }
-    })
-    
+    ans = [...new Set(answer)]
     ans.sort((a, b) => a - b)
-    return ans;
+    
+    return ans
 }
