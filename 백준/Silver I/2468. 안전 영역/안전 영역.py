59507ec51a659061a83e7f87cb9d9483a91342b1
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-14 16:19:48 +0900

    [Silver I] Title: 안전 영역, Time: 888 ms, Memory: 34132 KB -BaekjoonHub

---
+++ 백준/Silver I/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,57 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+max_val = 0
+min_val = float('inf')
+
+for _ in range(N):
+    line = list(map(int, input().split()))
+    max_val = max(max_val, max(line))
+    min_val = min(min_val, min(line))
+    arr.append(line)
+
+def sink(arr, sink_num):
+    visited = [[0 for _ in range(N)] for _ in range(N)]
+    
+    for n in range(len(arr)):
+        for m in range(len(arr)):
+            if sink_num >= arr[n][m]:
+                arr[n][m] == 0
+                visited[n][m] = float('inf')
+            else:
+                visited[n][m] = 0
+
+    return arr, visited
+
+def bfs(sn, sm, cnt):
+    q = deque([(sn, sm)])
+    visited[sn][sm] = cnt
+    while q:
+
+        x, y = q.popleft()
+        dir = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+        for n, m in dir:
+            nx, my = n + x,  m + y
+            if 0 <= nx < N and 0 <= my < N:
+                if visited[nx][my] == 0:
+                    visited[nx][my] = visited[x][y]
+                    q.append((nx, my))
+
+answer = 1
+for val in range(min_val, max_val+1):
+    arr, visited = sink(arr, val)
+    cnt = 1
+    
+    for n in range(len(arr)):
+        for m in range(len(arr)):
+            if visited[n][m] == 0:
+                bfs(n, m, cnt)
+                cnt += 1
+    answer = max(answer, cnt-1)
+
+print(answer)
+
