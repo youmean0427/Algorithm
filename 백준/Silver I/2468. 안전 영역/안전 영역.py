b2ed4cc0c7faa3fd69671c14d48466a4577aadb4
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-14 16:33:25 +0900

    [Silver I] Title: 안전 영역, Time: 772 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver I/2468. 안전 영역/안전 영역.py
+++ 백준/Silver I/2468. 안전 영역/안전 영역.py
@@ -15,45 +15,41 @@ for _ in range(N):
     arr.append(line)
 
 def sink(arr, sink_num):
-    visited = [[0 for _ in range(N)] for _ in range(N)]
+    visited = [[0] * N for _ in range(N)]
 
-    for n in range(len(arr)):
-        for m in range(len(arr)):
-            if sink_num >= arr[n][m]:
-                arr[n][m] == 0
-                visited[n][m] = float('inf')
-            else:
-                visited[n][m] = 0
+    for n in range(N):
+        for m in range(N):
+            if arr[n][m] <= sink_num:
+                arr[n][m] = 0
+                visited[n][m] = 1
 
-    return arr, visited
+    return visited
 
-def bfs(sn, sm, cnt, visited):
+def bfs(sn, sm, visited):
     q = deque([(sn, sm)])
-    visited[sn][sm] = cnt
+    visited[sn][sm] = 1
     while q:
-
         x, y = q.popleft()
         dir = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
         for n, m in dir:
             nx, my = n + x, m + y
-            if 0 <= nx < N and 0 <= my < N:
-                if visited[nx][my] == 0:
-                    visited[nx][my] = visited[x][y]
-                    q.append((nx, my))
+            if 0 <= nx < N and 0 <= my < N and not visited[nx][my]:
+                visited[nx][my] = 1
+                q.append((nx, my))
 
 def find(arr):
     answer = 1
-    for val in range(min_val, max_val + 1):
-        sink_arr, visited = sink(arr, val)
-        cnt = 1
+    for val in range(min_val, max_val):
+        visited = sink(arr, val)
+        cnt = 0
 
-        for n in range(len(arr)):
-            for m in range(len(arr)):
-                if visited[n][m] == 0:
-                    bfs(n, m, cnt, visited)
+        for n in range(N):
+            for m in range(N):
+                if not visited[n][m]:
+                    bfs(n, m, visited)
                     cnt += 1
-        answer = max(answer, cnt - 1)
+        answer = max(answer, cnt)
     return answer
 
 print(find(arr))
