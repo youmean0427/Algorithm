5b8bed0941cd9dc423c59dc053490c6adae67f77
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-18 02:06:50 +0900

    [level 1] Title: 소수 찾기, Time: 3015.28 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,18 +1,14 @@
 def solution(n):
     answer = 0
     
-    def check(n):
-        if n == 1:
-            return False
-
+    def prime(n):
         for i in range(2, int(n ** (1/2))+1):
             if n % i == 0:
                 return False
         return True
     
-    
-    for i in range(1, n+1):
-        if check(i):
+    for i in range(2, n+1):
+        if prime(i):
             answer += 1
 
     return answer
