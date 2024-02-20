c50efa6a68adf26264b4115e1df170992be4ccfb
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-20 13:58:25 +0900

    [level 1] Title: 소수 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,28 @@
+function solution(nums) {
+    var answer = 0
+    const N = nums.length
+    let visited = new Array(N).fill(0)
+    let dfs_nums = []
+    
+    function prime(n) {
+        for (let i = 2; i <= n ** (1/2); i++) {
+            if (n % i == 0) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    for (let a = 0; a < N; a++) {
+        for (let b = a+1; b < N; b++) {
+            for (let c = b+1; c < N; c++) {
+                let addNum = nums[a]+nums[b]+nums[c]
+                if (prime(addNum)) {
+                    answer += 1
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
