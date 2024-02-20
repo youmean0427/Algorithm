1481cfe94e59202be16a1512c759d62b5754bba8
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-20 14:08:49 +0900

    [level 1] Title: 소수 만들기, Time: 1.74 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,21 +1,24 @@
 function solution(nums) {
     var answer = 0
     const N = nums.length
+    let primeNums = new Array(3001).fill(0)
     
-    function prime(n) {
-        for (let i = 2; i <= n ** (1/2); i++) {
-            if (n % i == 0) {
-                return false
+    function prime() {
+        for (let i = 2; i < 3001; i++) {
+            let ii = i + i
+            while (ii < 3001) {
+                primeNums[ii] = 1
+                ii += i
             }
         }
-        return true
     }
+    prime()
     
     for (let a = 0; a < N; a++) {
         for (let b = a+1; b < N; b++) {
             for (let c = b+1; c < N; c++) {
                 let addNum = nums[a]+nums[b]+nums[c]
-                if (prime(addNum)) {
+                if (primeNums[addNum] == 0) {
                     answer += 1
                 }
             }
