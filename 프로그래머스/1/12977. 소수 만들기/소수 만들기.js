1037c0ee03b89ce37d0f3b52bd2c932bc5127417
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-23 21:11:59 +0900

    [level 1] Title: 소수 만들기, Time: 1.59 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,29 +1,35 @@
 function solution(nums) {
-    var answer = 0
-    const N = nums.length
-    let primeNums = new Array(3001).fill(0)
+    let answer = 0;
+    const N = nums.length;
+    let primeNums = new Array(3001).fill(0);
     
+    // 소수를 찾는 함수
     function prime() {
         for (let i = 2; i < 3001; i++) {
-            let ii = i + i
+            let ii = i + i;
             while (ii < 3001) {
-                primeNums[ii] = 1
-                ii += i
+                primeNums[ii] = 1;
+                ii += i;
             }
         }
     }
-    prime()
+    prime();
     
-    for (let a = 0; a < N; a++) {
-        for (let b = a+1; b < N; b++) {
-            for (let c = b+1; c < N; c++) {
-                let addNum = nums[a]+nums[b]+nums[c]
-                if (primeNums[addNum] == 0) {
-                    answer += 1
-                }
+    // DFS 함수
+    function dfs(idx, count, sum) {
+        if (count === 3) { // 세 개의 수를 더한 경우
+            if (primeNums[sum] === 0) { // 소수인지 확인
+                answer += 1;
             }
+            return;
+        }
+        
+        for (let i = idx; i < N; i++) {
+            dfs(i + 1, count + 1, sum + nums[i]); // 다음 숫자로 이동
         }
     }
+    
+    dfs(0, 0, 0); // DFS 시작
     
     return answer;
 }
