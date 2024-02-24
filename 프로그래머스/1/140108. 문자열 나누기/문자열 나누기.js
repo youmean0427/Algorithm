a84462a9fe51fd75fdf9c5c1b2f19da1b345a28b
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-25 08:37:59 +0900

    [level 1] Title: 문자열 나누기, Time: 0.51 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -5,20 +5,16 @@ function solution(s) {
     let otherCnt = 0
     
     for (let i = 1; i < s.length; i++) {
-
         if (charCnt === otherCnt) {
             char = s[i]
             charCnt = 1
             otherCnt = 0
             answer += 1
-        }
-        else if (char === s[i]) {
+        } else if (char === s[i]) {
             charCnt += 1
         } else {
             otherCnt += 1
         }
     }
-    
-    
     return answer;
 }
