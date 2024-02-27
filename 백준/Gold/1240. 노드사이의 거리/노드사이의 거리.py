4999d611aa5b5f6d88186b5c2808e21e4ed31855
YM <109326287+youmean0427@users.noreply.github.com>
2024-02-27 19:41:09 +0900

    [Gold V] Title: 노드사이의 거리, Time: 712 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
@@ -0,0 +1,34 @@
+import sys, heapq
+input = sys.stdin.readline
+
+def djik(start):
+
+    D = [float('inf')] * (N+1)
+    D[start] = 0
+
+    heap = []
+    heapq.heappush(heap, (0, start))
+
+    while heap:
+
+        cost, node = heapq.heappop(heap)
+
+        for next_cost, next_node in arr[node]:
+            cost_sum = next_cost + cost
+            if D[next_node] > cost_sum:
+                D[next_node] = cost_sum
+                heapq.heappush(heap, (cost_sum, next_node))
+
+    return D
+
+N, M = map(int, input().split())
+arr = [[] for _ in range(N+1)]
+
+for _ in range(N-1):
+    a, b, c = map(int, input().split())
+    arr[a].append((c, b)) # (cost, node)
+    arr[b].append((c, a))
+
+for _ in range(M):
+    start, end = map(int, input().split())
+    print(djik(start)[end])
