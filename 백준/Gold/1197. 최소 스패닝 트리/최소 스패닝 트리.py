9af479950f9282df1c0f306c608ba248787a8577
YM <109326287+youmean0427@users.noreply.github.com>
2024-03-03 14:39:35 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 276 ms, Memory: 49216 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,28 +1,31 @@
 import sys
+# sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+V, E = map(int, input().split())
+arr = []
+for _ in range(E):
+    A, B, C = map(int, input().split())
+    arr.append((A, B, C))
+
+p = [i for i in range(V+1)]
 
 def find_set(x):
     if p[x] != x:
         p[x] = find_set(p[x])
     return p[x]
 
-def union(x, y, z):
+def union(a, b, c):
     global result
-    if find_set(x) != find_set(y):
-        p[find_set(y)] = find_set(x)
-        result += z
+    if find_set(b) != find_set(a):
+        p[find_set(b)] = find_set(a)
+        result += c
 
-V, E = map(int, input().split())
-arr = []
-p = [int(i) for i in range(0, V+1)]
 result = 0
-for _ in range(E):
-    a, b, c = map(int, input().split())
-    arr.append([a, b, c])
+arr.sort(key=lambda x: x[2])
 
-arr.sort(key=lambda x:x[2])
-for a, b, c in arr:
+for a, b, c, in arr:
     union(a, b, c)
 
 print(result)
-
