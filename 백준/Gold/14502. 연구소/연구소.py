ea0771dd34f90585a8b460b3f436fc1493332d3b
YM <109326287+youmean0427@users.noreply.github.com>
2024-03-07 18:25:15 +0900

    [Gold IV] Title: 연구소, Time: 2324 ms, Memory: 33472 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -7,46 +7,52 @@ N, M = map(int, input().split())
 arr = []
 zero = []
 virus = []
-for i in range(N):
-    line = list(map(int, input().split()))
-    arr.append(line)
-    for j in range(len(line)):
-        if line[j] == 0:
-            zero.append((i, j))
-        if line[j] == 2:
-            virus.append((i, j))
 
-def bfs(sn, sm):
+def arr_input():
+
+    for i in range(N):
+        line = list(map(int, input().split()))
+        arr.append(line)
+        for j in range(len(line)):
+            if line[j] == 0:
+                zero.append((i, j))
+            elif line[j] == 2:
+                virus.append((i, j))
+
+def bfs(sn, sm, wall):
 
     q = [(sn, sm)]
-    visited[sn][sm] = 2
+    wall[sn][sm] = 2
+
     while q:
         x, y = q.pop(0)
-
         dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
         for n, m in dir:
             nx = n + x
             ym = y + m
             if 0 <= nx < N and 0 <= ym < M:
-                if visited[nx][ym] == 0:
-                    visited[nx][ym] = 2
+                if wall[nx][ym] == 0:
+                    wall[nx][ym] = 2
                     q.append((nx, ym))
 
+def wall_case():
+    ans = 0
+    zero_case = list(combinations(zero, 3))
+    for case in zero_case:
+        wall = copy.deepcopy(arr)
+        for n, m in case:
+            wall[n][m] = 1
 
-zero_case = list(combinations(zero, 3))
-ans = 0
-for case in zero_case:
-    wall = copy.deepcopy(arr)
+        for x, y in virus:
+            bfs(x, y, wall)
 
-    for n, m in case:
-        wall[n][m] = 1
-    visited = copy.deepcopy(wall)
-    for x, y in virus:
-        bfs(x, y)
-    total = 0
-    for v in visited:
-        total += v.count(0)
+        total = 0
+        for v in wall:
+            total += v.count(0)
 
-    ans = max(ans, total)
+        ans = max(ans, total)
+    return ans
 
-print(ans)
+arr_input()
+print(wall_case())
