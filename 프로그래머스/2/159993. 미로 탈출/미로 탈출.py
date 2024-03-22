314f9e6ac5b2a8a47753fdd921d43d2f2c0ad476
YM <109326287+youmean0427@users.noreply.github.com>
2024-03-22 23:58:17 +0900

    [level 2] Title: 미로 탈출, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,32 +1,27 @@
 def solution(maps):
     answer = -1
-    
-    start = []
-    lever = []
-    end = []
-    N = len(maps)
-    M = len(maps[0])
+
+    N, M = len(maps), len(maps[0])
     for n in range(N):
         for m in range(M):
             if maps[n][m] == "S":
-                start.append((n, m))
+                start = (n, m)
             elif maps[n][m] == "L":
-                lever.append((n, m))
+                lever = (n, m)
             elif maps[n][m] == "E":
-                end.append((n, m))
-
+                end = (n, m)
+                
     def bfs(sn, sm, en, em):
         visited = [[float("inf") for _ in range(M)] for _ in range(N)]
         q = [(sn, sm)]
         visited[sn][sm] = 1
+        
         while q:
             x, y = q.pop(0)
             if x == en and y == em:
                 return visited[en][em]
             
-            
             dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-            
             for n, m in dir:
                 nx, my = n+x, m+y
                 if 0 <= nx < N and 0 <= my < M:
@@ -35,12 +30,10 @@ def solution(maps):
                             visited[nx][my] = min(visited[nx][my], visited[x][y]+1)
                             q.append((nx, my))
 
-    start_to_lever = bfs(start[0][0], start[0][1], lever[0][0], lever[0][1])
-    lever_to_end = bfs(lever[0][0], lever[0][1], end[0][0], end[0][1])
+    start_to_lever = bfs(start[0], start[1], lever[0], lever[1])
+    lever_to_end = bfs(lever[0], lever[1], end[0], end[1])
                 
-    if start_to_lever == None or lever_to_end == None:
-        pass
-    else:
+    if start_to_lever != None and lever_to_end != None:
         answer = start_to_lever - 1 + lever_to_end - 1
     
     return answer
