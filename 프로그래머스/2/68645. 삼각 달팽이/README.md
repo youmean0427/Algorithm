f011eb22fb0b53f9846081206aa9e851abcc03e4
YM <109326287+youmean0427@users.noreply.github.com>
2024-04-21 20:24:57 +0900

    [level 2] Title: 삼각 달팽이, Time: 3267.22 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/README.md
+++ 프로그래머스/2/68645. 삼각 달팽이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 삼각 달팽이 - 68645 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=javascript) 
 
 ### 성능 요약
 
-메모리: 42.9 MB, 시간: 335.52 ms
+메모리: 125 MB, 시간: 3267.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 21일 20:06:09
+2024년 04월 21일 20:24:55
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -0,0 +1,46 @@
+function solution(n) {
+    var answer = [];
+    let arr = Array.from({length : n}, () => new Array(n).fill(0))
+    let  exit = 0
+    for (let i = 1; i <= n; i++) {
+        exit += i
+    }
+    
+    function dfs(sn, sm) {
+        
+        arr[sn][sm] = 1
+        let stack = [[sn, sm]]
+        let dir_idx = 0
+        const dir = [[1, 0], [0, 1], [-1, -1]]
+        const dir_chan = {0 : 1, 1: 2, 2: 0}
+        
+        while (stack.length) {
+            const [x, y] = stack.pop()
+            const [dn, dm] = dir[dir_idx]
+            
+            if (arr[x][y] === exit) {
+                return 
+            }
+            
+            const xn = x + dn
+            const ym = y + dm
+            
+            if ( 0 <= xn && xn < n && 0<= ym && ym < n) {
+                if (arr[xn][ym] === 0) {
+                arr[xn][ym] = arr[x][y] + 1
+                stack.push([xn, ym])
+                continue
+                }
+            }
+            dir_idx = dir_chan[dir_idx]
+            stack.push([x, y])
+        }
+    }
+    dfs(0, 0)
+    
+    for (let i = 0; i < n; i++) {
+        const filter = arr[i].filter(x => x > 0)
+        answer = [...answer, ...filter]
+    }
+    return answer;
+}
