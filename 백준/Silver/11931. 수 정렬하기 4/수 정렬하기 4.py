2a9a0a2a613689cc02ab2403000ef230e881ac52
YM <109326287+youmean0427@users.noreply.github.com>
2024-09-07 17:14:36 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 800 ms, Memory: 240012 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -6,6 +6,40 @@ arr = []
 for _ in range(n):
     arr.append(int(input()))
 
-arr.sort(reverse=True)
+negative = []
+positive = []
+
 for i in arr:
+    if i < 0:
+        negative.append(-i)
+    else:
+        positive.append(i)
+
+def radix_sort(arr, n):
+    # Radix Sort
+    k = 7
+    p = 1
+    for i in range(k):
+        nums = [[] for _ in range(10)]
+
+        for j in range(n):
+            x = (arr[j] // p) % 10
+            nums[x].append(arr[j])
+
+        sorted_arr = []
+        for a in range(9, -1, -1):
+            for b in range(len(nums[a])):
+                sorted_arr.append(nums[a][b])
+
+        p *= 10
+        arr = sorted_arr
+
+    return arr
+
+negative = radix_sort(negative, len(negative))
+positive = radix_sort(positive, len(positive))
+
+for i in positive:
     print(i)
+for i in negative[::-1]:
+    print(-i)
