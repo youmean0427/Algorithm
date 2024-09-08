6219af39a88f88106826320c85baf644d9494f04
YM <109326287+youmean0427@users.noreply.github.com>
2024-09-08 19:08:49 +0900

    [Silver I] Title: 곱셈, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -2,5 +2,15 @@ import sys
 input = sys.stdin.readline
 
 A, B, C = map(int, input().split())
-x = pow(A, B, C)
-print(x)
+
+def power(a, n):
+    if n == 0:
+        return 1
+    else:
+        x = power(a, n//2)
+        if (n % 2 == 0):
+            return (x * x) % C
+        else:
+            return (x * x * a) % C
+
+print(power(A, B))
